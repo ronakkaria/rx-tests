7635784923e3b2175d08bddeb9a86933913c2050
Ronak Karia <ronakkaria06@gmail.com>
2015-04-11 14:24:10 +0530

    removed onSubscribe interface

--- src/ronak/apps/tdd/AppUtils.java
+++ src/ronak/apps/tdd/AppUtils.java
@@ -2,13 +2,11 @@ package ronak.apps.tdd;
 
 import android.widget.TextView;
 
-import rx.Observable.OnSubscribe;
 import rx.Observer;
-import rx.Subscriber;
 import rx.android.widget.OnTextChangeEvent;
 import rx.functions.Func1;
 
-public class AppUtils implements OnSubscribe<String>{
+public class AppUtils {
 
 	public static void bindViews(TextView tv, ObservableEditText et) {
 		TextViewObserver tObserver = new TextViewObserver(tv);
@@ -34,12 +32,6 @@ public class AppUtils implements OnSubscribe<String>{
 
 		}).subscribe(ob);
 		
-	}
-
-	@Override
-	public void call(Subscriber<? super String> arg0) {
-		// TODO Auto-generated method stub
-		arg0.onNext("response");
 	}
 	
 	
